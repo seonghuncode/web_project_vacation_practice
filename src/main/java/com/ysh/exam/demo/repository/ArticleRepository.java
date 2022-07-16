96414f954533fa69c2a3c466a587fa611c572e95
seonghuncode <gnos456@naver.com>
2022-07-16 22:40:55 +0900

    DELETE, UPDATE, SELECT 관련 쿼리를 메서드와 연결

--- src/main/java/com/ysh/exam/demo/repository/ArticleRepository.java
+++ src/main/java/com/ysh/exam/demo/repository/ArticleRepository.java
@@ -2,9 +2,12 @@ package com.ysh.exam.demo.repository;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import com.ysh.exam.demo.vo.Article;
 
@@ -28,20 +31,24 @@ public interface ArticleRepository {
 	//(하지만 마이바티스가 알아서 할 수 있도록 힌트를 주어야 한다) -> @Select, @Parm 을 사용하여
 
 	
-	//INSERT INTO article SET regDate = NOW(), updateDate = NOW(), title = >, body = ?
-	public Article writeArticle(String title, String body );
+	//INSERT INTO article SET regDate = NOW(), updateDate = NOW(), title = ?, body = ?
+	@Insert("INSERT INTO article SET regDate = NOW(), updateDate = NOW(), title = #{title}, `body` = #{body}")
+	public Article writeArticle(@Param("title") String title,@Param("body") String body );
 
 	//(SELECT * FROM article WHERE id = ?) 이러한 쿼리가 실행이 되야 기능이 동작한다
-	@Select("select * from article where id = #{id}") // == 아래의 메서드가 실행 되면 이와 같은 쿼리문이 실행 된다 id는 아래의 id를 받는다. 실행하고 Article에 담아서 리턴을 한다
+	@Select("SELECT * FROM article WHERE id = #{id}") // == 아래의 메서드가 실행 되면 이와 같은 쿼리문이 실행 된다 id는 아래의 id를 받는다. 실행하고 Article에 담아서 리턴을 한다
 	public Article getArticle(@Param("id") int id);
 	
 	//(DELETE * FROM article WHERE id = ?)
-	public void deleteArticle(int id);
+	@Delete("DELETE FROM article WHERE id = #{id}")
+	public void deleteArticle(@Param("id") int id);
 	
 	//(UPDATE article SET title = ?, body = ?, updateDate = NOW() WHERE id = ?)
-	public void modifyArticle(int id, String title, String body);
+	@Update("UPDATE article SET title = #{title}, `body` = #{body}, updateDate = NOW() WHERE id = #{id}")
+	public void modifyArticle(@Param("id" )int id,@Param("title") String title,@Param("body") String body);
 
 	//(SELECT * FROM article ORDER BY id DESC)
+	@Select("SELECT * FROM article ORDER BY id DESC")
 	public List<Article> getArticles();
 	
 	
