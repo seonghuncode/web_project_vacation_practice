9e00a4758d5e8084dbf4ddb45459cbe044a84eeb
seonghuncode <gnos456@naver.com>
2022-07-22 23:14:16 +0900

    JSP 적용

--- src/main/java/com/ysh/exam/demo/controller/UsrHomeController.java
+++ src/main/java/com/ysh/exam/demo/controller/UsrHomeController.java
@@ -16,9 +16,20 @@ public class UsrHomeController {
 	
 	
 	@RequestMapping("/usr/home/main")
-	@ResponseBody
-	public String getString() {
-		return "안녕하세요.";
+	public String showMain() {
+		//return "/WEB-INF/jsp/usr/home/main.jsp"; //JSP를 사용하기 위해서는 @ResponseBody를 빼고 실제 경로를 적어준다
+		//(하지만 application.yml의 prefix에 /WEB-INF/jsp/를 적어 주었기 때문에 생략하고 아래처럼 사용 가능)
+		//suffix를 사용한 결과 .jsp생략 가능
+		return "/usr/home/main";
+	}
+	
+	
+	@RequestMapping("/")
+	public String showRoot() {
+		return "redirect:/usr/home/main";
+		
+		//url에서 "/"이러한 주소로 들어오면 자동으로 /usr/home/main으로 이동한다
+		//@ResponseBody를뺀다.
 	}
 	
 	
---
+++ src/main/webapp/WEB-INF/jsp/usr/home/main.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>메인</title>
+</head>
+<body>
+  <h1>안녕하세요.</h1>
+  <div>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum quo quae quibusdam voluptatum ullam eaque corporis minima iste. Quisquam nobis mollitia laboriosam molestias enim earum et perspiciatis alias voluptatum error.
+  </div>
+</body>
+</html>
